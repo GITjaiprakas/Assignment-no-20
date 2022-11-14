2979fbbedaf978e5a9e9a07d2aa1f6f3fb3c0179
GITjaiprakas <107695687+GITjaiprakas@users.noreply.github.com>
2022-11-14 18:12:13 +0530

    Update 4.c

--- 4.c
+++ 4.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
 int main(){
-    int a=4,b=1,*p;
-    p=&a;//assigning the address of a variable to a pointer p
-    printf("%d",*p);
+    int x=10,*p,**q,***r;
+    p=&x;//assigning the address of x variable to a pointer p
+    q=&p;//assigning the address of pointer to integer variable p variable to a pointer q
+    r=&q;//assigning the address of pointer to pointer to pointer to integer variable q variable to a pointer r
+    printf("%d %d %d %d",x,*p,**q,***r);
+    printf("%d %d %d %d",&x,p,*q,**r);
+    printf("%d %d %d %d",&p,q,*r);
+    printf("%d",&r);
     return 0;
 }
